576735b8bf61a58096b864ba9aeff4d429552b95
Axel <mail@axel-schulze.net>
2023-01-13 13:12:01 +0100

    Update to 1.0.10

---
+++ src/DocX.ConsoleTest/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Novacode;
+
+var doc = DocX.Create(@"MyFile.docx");
+var p = doc.InsertParagraph();
+p.Append("Hello World");
+var i = doc.AddImage("purple.png");
+var p2 = doc.InsertParagraph();
+p2.AppendPicture(i.CreatePicture());
+
+doc.AddHeaders();
+var pic = i.CreatePicture();
+var hp = doc.Headers.odd.InsertParagraph();
+hp.InsertPicture(pic);
+hp.InsertParagraphBeforeSelf(doc.InsertParagraph());
+
+doc.Save();
